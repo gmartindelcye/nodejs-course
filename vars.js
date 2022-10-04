b0e56ba37eeee6ed2ec0266962cc0e463ed4625a
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2022-10-03 22:28:17 -0500

    console, variables

---
+++ vars.js
@@ -0,0 +1,20 @@
+let username = 'gmartin'
+let age = 58
+let hashobbies = false
+let points = [10, 20, 30]
+let user = {
+    name: 'Ryan',
+    lastname: 'Benton'
+}
+const PI = 3.1416
+
+
+console.log(username)
+console.log(age)
+console.log(hashobbies)
+console.log(points)
+console.log(user)
+console.log(PI)
+
+
+
